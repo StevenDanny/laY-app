4837701b4f869bef83bef1ed604707d9f7ba2cbd
Steven <stevendanny2000@gmail.com>
2021-01-07 17:46:01 -0500

    primer commit

---
+++ frontend/src/data.js
@@ -0,0 +1,39 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nombre de prueba',
+            category: 'Categoria de prueba',
+            image: '/images/producto.png',
+            price: 0.35,
+            brand: 'Marca de prueba',
+            rating: 5,
+            numReviews: 35,
+            description: 'Descripcion de prueba'
+        },
+        {
+            _id: '2',
+            name: 'Nombre de prueba2',
+            category: 'Categoria de prueba',
+            image: '/images/producto.png',
+            price: 0.35,
+            brand: 'Marca de prueba',
+            rating: 5,
+            numReviews: 35,
+            description: 'Descripcion de prueba'
+        },
+        {
+            _id: '3',
+            name: 'Nombre de prueba3',
+            category: 'Categoria de prueba',
+            image: '/images/producto.png',
+            price: 0.35,
+            brand: 'Marca de prueba',
+            rating: 5,
+            numReviews: 35,
+            description: 'Descripcion de prueba'
+        }
+    ]
+}
+
+export default data
