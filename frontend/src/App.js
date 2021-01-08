1728291bbbe638355451193d3cfbe75ab9c0058a
Steven <stevendanny2000@gmail.com>
2021-01-08 17:26:22 -0500

    react route | HomeScreen and ProductSrceen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,30 @@
 import React from 'react'
-import data from './data'
-import Product from './components/Product'
+import { BrowserRouter, Route } from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App(props) {
     return (
-        <div className="grid-container">
-            <header className="row">
-                <div>
-                    <a className="brand" href="/"> La "Y" </a>
-                </div>
-                <div>
-                    <a href="/cart">Cesta</a>
-                    <a href="/signin">Loguearse</a>
-                </div>
-            </header>
-            <main>
-                <div className="row center">
-                    {
-                        data.products.map((product) => (
-                            <Product key={product._id} product={product} />
-                        ))
-                    }
-                </div>
-            </main>
-            <footer className="row center">
-                All right reserved
+        <BrowserRouter>
+            <div className="grid-container">
+                <header className="row">
+                    <div>
+                        <a className="brand" href="/"> La "Y" </a>
+                    </div>
+                    <div>
+                        <a href="/cart">Cesta</a>
+                        <a href="/signin">Loguearse</a>
+                    </div>
+                </header>
+                <main>
+                    <Route path="/product/:id" component={ProductScreen}></Route>
+                    <Route path="/" component={HomeScreen} exact></Route>
+                </main>
+                <footer className="row center">
+                    All right reserved
             </footer>
-        </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
