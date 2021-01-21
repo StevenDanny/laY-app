0a7139e13d529e14f75247e6e1dab8a09d523f0b
Steven <stevendanny2000@gmail.com>
2021-01-21 05:48:45 -0500

    signin backend

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,32 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import productRouter from './routers/productRouter.js';
-import userRouter from './routers/userRouter.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import productRouter from "./routers/productRouter.js";
+import userRouter from "./routers/userRouter.js";
+
+dotenv.config();
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/laY', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/laY", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
-app.use('/api/users', userRouter);
+app.use("/api/users", userRouter);
 
-app.use('/api/products', productRouter);
+app.use("/api/products", productRouter);
 
-app.get('/', (req, res) => res.send('Server is ready'));
+app.get("/", (req, res) => res.send("Server is ready"));
 
 app.use((err, req, res, next) => {
-    res.status(500).send({ message: err.message });
-})
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 
